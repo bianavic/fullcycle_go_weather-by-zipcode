da22f9193a926c08b28e42234de69886a05cb877
bianavic <bianavic@gmail.com>
2026-06-26 12:33:27 -0300

    feat(domain): add Temperature value object with C/F/K conversions

--- internal/domain/temperature.go
+++ internal/domain/temperature.go
@@ -1,1 +1,21 @@
 package domain
+
+type Temperature struct {
+	celsius float64
+}
+
+func FromCelsius(c float64) Temperature {
+	return Temperature{celsius: c}
+}
+
+func (t Temperature) Celsius() float64 {
+	return t.celsius
+}
+
+func (t Temperature) Fahrenheit() float64 {
+	return t.celsius*1.8 + 32
+}
+
+func (t Temperature) Kelvin() float64 {
+	return t.celsius + 273
+}
