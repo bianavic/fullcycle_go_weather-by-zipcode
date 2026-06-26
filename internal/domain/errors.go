3812af351cdc94dfadbd9a1dba770ef3f65cea96
bianavic <bianavic@gmail.com>
2026-06-26 12:30:45 -0300

    feat(domain): add Zipcode value object and ErrInvalidZipcode

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -1,1 +1,5 @@
 package domain
+
+import "errors"
+
+var ErrInvalidZipcode = errors.New("invalid zipcode")
--- internal/domain/zipcode.go
+++ internal/domain/zipcode.go
@@ -1,1 +1,26 @@
 package domain
+
+import "regexp"
+
+type Zipcode string
+
+var zipcodePattern = regexp.MustCompile(`^[0-9]{8}$`)
+
+func NewZipcode(s string) (Zipcode, error) {
+	z := Zipcode(s)
+	if err := z.Validate(); err != nil {
+		return "", err
+	}
+	return z, nil
+}
+
+func (z Zipcode) Validate() error {
+	if !zipcodePattern.MatchString(string(z)) {
+		return ErrInvalidZipcode
+	}
+	return nil
+}
+
+func (z Zipcode) String() string {
+	return string(z)
+}
