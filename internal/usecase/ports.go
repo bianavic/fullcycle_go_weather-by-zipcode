4ea9256b9a2d3127296166ea9154eaf6f45e13ef
bianavic <bianavic@gmail.com>
2026-06-26 12:52:02 -0300

    feat(usecase): add WeatherByZipcode orchestrator with ports and DTO

--- internal/usecase/ports.go
+++ internal/usecase/ports.go
@@ -1,1 +1,15 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/bianavic/fullcycle_go_weather-by-zipcode/internal/domain"
+)
+
+type CityFinder interface {
+	Find(ctx context.Context, zipcode domain.Zipcode) (city string, err error)
+}
+
+type WeatherFinder interface {
+	CurrentTempC(ctx context.Context, city string) (float64, error)
+}
